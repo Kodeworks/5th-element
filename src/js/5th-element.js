6b011dbba6bccfb7c0d66aa802a949191b8ea0d8
Tri Nguyen <mail@trimn.net>
2021-05-05 20:18:32 +0200

    Modify animation and refactor 5th-element

--- src/js/5th-element.js
+++ src/js/5th-element.js
@@ -1,29 +1,19 @@
-/**
- * 5th Element :)
- *
- * Adjustable parameters
- * - people
- * - gridLengthInMinutes
- *
- */
-
-const columnInput = document.getElementById('columns');
-const rowInput = document.getElementById('rows');
-
 const columnConter = document.getElementById('columnConter');
-const rowConter = document.getElementById('rowConter');
+const columnInput = document.getElementById('columns');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
 
-const gridWidth = 600;
+const conversations = [];
+const groups = [];
 const gridHeight = 800;
+const gridWidth = 600;
 
 let seconds = 5;
-let people = 3;
 let gridLengthInMinutes = 15;
+let people = 5;
 
-const startTime = Math.floor(new Date().getTime() / 1000);
-
-const groups = [];
-const conversations = [];
+let drawInterval;
+let socket;
 
 let grid = d3
   .select('#grid')
@@ -33,19 +23,47 @@ let grid = d3
   .attr('xmlns', 'http://www.w3.org/2000/svg');
 
 columnConter.innerText = people;
-rowCounter.innerText = gridLengthInMinutes;
+columnInput.value = people;
 
-// Handlers
+/* Listeners
+-----------------------------------------------------------------------------*/
+columnInput.addEventListener('input', handleColumnChange);
+startButton.addEventListener('click', connect);
+stopButton.addEventListener('click', disconnect);
+
+/* Initialize
+-----------------------------------------------------------------------------*/
+function initialize() {
+  connect();
+  drawCircles();
+}
+initialize();
+
+/* Handlers
+-----------------------------------------------------------------------------*/
 function handleColumnChange(event) {
   people = event.target.value;
   columnConter.innerText = people;
   handleRedraw();
 }
 
-function handleRowChange(event) {
-  gridLengthInMinutes = event.target.value;
-  rowCounter.innerText = gridLengthInMinutes;
-  handleRedraw();
+function handleConversation(conversation) {
+  conversations.push(conversation);
+
+  if (conversations.length >= gridLengthInMinutes) {
+    conversations.shift();
+  }
+
+  if (d3.selectAll('g.drawGroup').size() >= gridLengthInMinutes * 2) {
+    d3.selectAll('g.drawGroup').each(function (group, index) {
+      if (index == 0) {
+        d3.select(this).remove();
+        return false;
+      }
+    });
+  }
+
+  draw(conversation);
 }
 
 function handleRedraw() {
@@ -58,54 +76,31 @@ function handleRedraw() {
     .attr('height', gridHeight)
     .attr('xmlns', 'http://www.w3.org/2000/svg');
 
-  // conversations.forEach((conversation) => draw(conversation));
-
   drawCircles();
 }
 
-// Listeners
-columnInput.addEventListener('input', handleColumnChange);
-rowInput.addEventListener('input', handleRowChange);
-
-drawCircles();
-
-function drawCircles() {
-  const circleGroup = grid.append('g').attr('class', 'circles');
-
-  for (let i = 0; i <= gridLengthInMinutes; i++) {
-    for (let j = 0; j <= people; j++) {
-      circleGroup
-        .append('circle')
-        .attr('stroke', 'none')
-        .attr('fill', '#efefef')
-        .attr('cx', (gridWidth / people) * j + 5)
-        .attr('cy', (gridHeight / gridLengthInMinutes) * i + 5)
-        .attr('r', 4);
-    }
-  }
+function handleRowChange(event) {
+  gridLengthInMinutes = event.target.value;
+  handleRedraw();
 }
 
-function transitionGrid() {
-  d3.selectAll('g.drawGroup').each(function () {
-    d3.select(this)
-      .transition()
-      .duration(seconds * 1000)
-      .attr(
-        'transform',
-        `translate(0, ${
-          gridHeight / gridLengthInMinutes +
-          this.transform.baseVal.consolidate().matrix.f
-        })`
-      );
-  });
+/* Seismograph
+-----------------------------------------------------------------------------*/
+function createIntercation(from, to, receiverIndex = 0) {
+  return [
+    [(gridWidth / people) * from + 5, 5],
+    [
+      (gridWidth / people) * to + 5,
+      (gridHeight / gridLengthInMinutes) * receiverIndex + 5,
+    ],
+  ];
 }
 
 function draw(conversation) {
   const group = grid
     .append('g')
     .attr('class', 'drawGroup')
-    .attr('stroke', 'black')
-    .attr('stroke-width', Math.floor(Math.random() * 2) == 0 ? 2 : 8)
+
     .attr('transform', 'translate(0, 0)')
     .attr('fill', 'none');
 
@@ -116,6 +111,11 @@ function draw(conversation) {
 
     const path = group
       .append('path')
+      .attr('stroke-width', Math.floor(Math.random() * 6) < 5 ? 2 : 8)
+      .attr(
+        'stroke',
+        Math.floor(Math.random() * 10) < 9 ? 'black' : 'rgb(200, 252, 160)'
+      )
       .attr(
         'd',
         d3.line()(
@@ -144,188 +144,69 @@ function draw(conversation) {
   }
 }
 
-function createIntercation(from, to, receiverIndex = 0) {
-  return [
-    [(gridWidth / people) * from + 5, 5],
-    [
-      (gridWidth / people) * to + 5,
-      (gridHeight / gridLengthInMinutes) * receiverIndex + 5,
-    ],
-  ];
+function drawCircles() {
+  const circleGroup = grid.append('g').attr('class', 'circles');
+
+  for (let i = 0; i <= gridLengthInMinutes; i++) {
+    for (let j = 0; j <= people; j++) {
+      circleGroup
+        .append('circle')
+        .attr('stroke', 'none')
+        .attr('fill', 'transparent')
+        .attr('cx', (gridWidth / people) * j + 5)
+        .attr('cy', (gridHeight / gridLengthInMinutes) * i + 5)
+        .attr('r', 4);
+    }
+  }
 }
 
-//
-// Controllers
-//
-const stopButton = document.getElementById('stopButton');
+function startTransitionInterval() {
+  drawInterval = setInterval(() => {
+    transitionGrid();
+  }, seconds * 1000);
+}
 
-//
-// Websocket
-//
-const socket = new WebSocket(
-  `ws://io.kodeworks.no/api/ws?rowseconds=${seconds}`
-);
+function transitionGrid() {
+  d3.selectAll('g.drawGroup').each(function () {
+    d3.select(this)
+      .transition()
+      .ease(d3.easeLinear)
+      .duration(seconds * 1000)
+      .attr(
+        'transform',
+        `translate(0, ${
+          gridHeight / gridLengthInMinutes +
+          this.transform.baseVal.consolidate().matrix.f
+        })`
+      );
+  });
+}
 
-socket.addEventListener('open', function (event) {
-  console.log('Connected to server');
-});
+/* Websocket
+-----------------------------------------------------------------------------*/
+function connect() {
+  console.log('Ready state', socket);
+  if (!socket || socket.readyState == 3) {
+    socket = new WebSocket(`ws://io.kodeworks.no/api/ws?rowseconds=${seconds}`);
 
-socket.addEventListener('message', function (event) {
-  socket.send('{"type":"ack"}');
-
-  const conversation = JSON.parse(event.data);
-  conversations.push(conversation);
-
-  if (conversations.length >= gridLengthInMinutes) {
-    conversations.shift();
-  }
-
-  if (d3.selectAll('g.drawGroup').size() >= gridLengthInMinutes * 2) {
-    d3.selectAll('g.drawGroup').each(function (group, index) {
-      if (index == 0) {
-        d3.select(this).remove();
-        return false;
-      }
+    socket.addEventListener('open', function (event) {
+      console.log('Connected to server');
     });
+
+    socket.addEventListener('message', function (event) {
+      socket.send('{"type":"ack"}');
+      handleConversation(JSON.parse(event.data));
+    });
+
+    socket.addEventListener('close', function (event) {
+      console.log('lol u got kickd from server', event);
+    });
+
+    startTransitionInterval();
   }
+}
 
-  draw(conversation);
-});
-
-socket.addEventListener('close', function (event) {
-  console.log('lol u got kickd from server', event);
-});
-
-const drawInterval = setInterval(() => {
-  transitionGrid();
-}, seconds * 1000);
-
-stopButton.addEventListener('click', function () {
+function disconnect() {
   socket.close();
   clearInterval(drawInterval);
-});
-
-function downloadPng(fileName) {
-  const svg = document.querySelector('svg');
-  svg.style.background = '#ffffff';
-
-  const copy = svg.cloneNode(true);
-
-  const canvas = document.createElement('canvas');
-
-  const bbox = svg.getBBox();
-
-  canvas.width = gridWidth;
-  canvas.height = gridHeight;
-
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, bbox.width, bbox.height);
-
-  const data = new XMLSerializer().serializeToString(copy);
-
-  const img = new Image();
-  const svgBlob = new Blob([data], { type: 'image/svg+xml;charset=utf-8' });
-  const url = URL.createObjectURL(svgBlob);
-
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-    URL.revokeObjectURL(url);
-
-    const imgURI = canvas
-      .toDataURL('image/png')
-      .replace('image/png', 'image/octet-stream');
-    triggerDownload(imgURI, fileName);
-  };
-
-  img.src = url;
 }
-
-function triggerDownload(imgURI, fileName) {
-  const event = new MouseEvent('click', {
-    view: window,
-    bubbles: false,
-    cancelable: true,
-  });
-
-  const anchor = document.createElement('a');
-  anchor.setAttribute('download', fileName);
-  anchor.setAttribute('href', imgURI);
-  anchor.setAttribute('target', '_blank');
-  anchor.dispatchEvent(event);
-}
-
-const downloadButton = document.getElementById('downloadButton');
-downloadButton.addEventListener('click', () => downloadPng('fil.png'));
-
-// const lol = [
-//   {
-//     time: 1618474794,
-//     interactions: [
-//       {
-//         sender: 11,
-//         receiver: 0,
-//         time: 1618474744,
-//       },
-//     ],
-//   },
-//   {
-//     time: 1618474774,
-//     interactions: [
-//       {
-//         sender: 10,
-//         receiver: 9,
-//         time: 1618474754,
-//       },
-//     ],
-//   },
-//   {
-//     time: 1618474772,
-//     interactions: [
-//       {
-//         sender: 7,
-//         receiver: 7,
-//         time: 1618474764,
-//       },
-//     ],
-//   },
-//   {
-//     time: 1618474764,
-//     interactions: [
-//       {
-//         sender: 7,
-//         receiver: 7,
-//         time: 1618474756,
-//       },
-//     ],
-//   },
-//   {
-//     time: 1618474754,
-//     interactions: [
-//       {
-//         sender: 10,
-//         receiver: 9,
-//         time: 1618474746,
-//       },
-//       {
-//         sender: 9,
-//         receiver: 10,
-//       },
-//     ],
-//   },
-//   {
-//     time: 1618474746,
-//     interactions: [
-//       {
-//         sender: 10,
-//         receiver: 10,
-//         time: 1618474742,
-//       },
-//     ],
-//   },
-// ];
-
-// const interval = setInterval(() => {
-//   const conv = lol.pop();
-//   conversations.push(conv);
-//   draw(conv);
-// }, seconds * 1000);
